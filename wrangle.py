069beb2f8b3bf71fa26e155ece5906fd1f30be20
Daniel Northcutt <daniel.h.northcutt@gmail.com>
2021-12-02 14:58:43 -0600

    Imports from review - my code commented out

--- wrangle.py
+++ wrangle.py
@@ -4,54 +4,236 @@ import env
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
-#----------------------------------------------------------#
-#get connection
 
-def get_db_url(url):
-    url = f'mysql+pymysql://{user}:{password}@{host}/{url}'
-    return url
+'''Wrangles data from Zillow Database'''
 
-#----------------------------------------------------------#
-#get zillow from SQL
+##################################################Wrangle.py###################################################
 
-def new_zillow_data():
-    '''
-    This function reads the titanic data from the Codeup db into a df,
-    write it to a csv file, and returns the df.
-    '''
-    # Create SQL query.
-    sql_query = """
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+from env import user, password, host
+
+#**************************************************Acquire*******************************************************
+
+def acquire_zillow():
+    ''' Acquire data from Zillow using env imports and rename columns'''
+    
+    url = f"mysql+pymysql://{user}:{password}@{host}/zillow"
+    
+    query = """
             
     SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
     FROM properties_2017
+
     LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+
     WHERE propertylandusedesc IN ("Single Family Residential",                       
                                   "Inferred Single Family Residential")"""
 
+    # get dataframe of data
+    df = pd.read_sql(query, url)
     
-    df = pd.read_sql(sql_query, get_db_url('zillow'))
-
+    
+    # renaming column names to one's I like better
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'area',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'year_built',})
     return df
 
-#----------------------------------------------------------#
-#def function + csv
+#**************************************************Remove Outliers*******************************************************
 
-def acquire_zillow():
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
     '''
-    This function reads in titanic data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
-    '''
-    if os.path.isfile('zillow_df.csv'):
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
         
-        # If csv file exists, read in data from csv file.
-        df = pd.read_csv('zillow_df.csv', index_col=0)
+        iqr = q3 - q1   # calculate interquartile range
         
-    else:
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
         
-        # Read fresh data from db into a DataFrame.
-        df = new_zillow_data()
-        
-        # Write DataFrame to a csv file.
-        df.to_csv('zillow_df.csv')
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
         
     return df
+
+#**************************************************Distributions*******************************************************
+
+def get_hist(df):
+    ''' Gets histographs of acquired continuous variables'''
+    
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = [col for col in df.columns if col not in ['fips', 'year_built']]
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot nos should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # turn off scientific notation
+        plt.ticklabel_format(useOffset=False)
+
+        plt.tight_layout()
+
+    plt.show()
+        
+        
+def get_box(df):
+    ''' Gets boxplots of acquired continuous variables'''
+    
+    # List of columns
+    cols = ['bedrooms', 'bathrooms', 'area', 'tax_value', 'taxamount']
+
+    plt.figure(figsize=(16, 3))
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display boxplot for column.
+        sns.boxplot(data=df[[col]])
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # sets proper spacing between plots
+        plt.tight_layout()
+
+    plt.show()
+        
+#**************************************************Prepare*******************************************************
+
+def prepare_zillow(df):
+    ''' Prepare zillow data for exploration'''
+
+    # removing outliers
+    df = remove_outliers(df, 1.5, ['bedrooms', 'bathrooms', 'area', 'tax_value', 'taxamount'])
+    
+    # get distributions of numeric data
+    get_hist(df)
+    get_box(df)
+    
+    # converting column datatypes
+    df.fips = df.fips.astype(object)
+    df.year_built = df.year_built.astype(object)
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    # impute year built using mode
+    imputer = SimpleImputer(strategy='median')
+
+    imputer.fit(train[['year_built']])
+
+    train[['year_built']] = imputer.transform(train[['year_built']])
+    validate[['year_built']] = imputer.transform(validate[['year_built']])
+    test[['year_built']] = imputer.transform(test[['year_built']])       
+    
+    return train, validate, test    
+
+#**************************************************Wrangle*******************************************************
+
+def wrangle_zillow():
+    '''Acquire and prepare data from Zillow database for explore'''
+    train, validate, test = prepare_zillow(acquire_zillow())
+    
+    return train, validate, test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #----------------------------------------------------------#
+# #get connection
+
+# def get_connection(db, user=env.user, host=env.host, password=env.password):
+#     connection_info = f'mysql+pymysql://{user}:{password}@{host}/{db}'
+#     return connection_info
+
+
+# #----------------------------------------------------------#
+# #get zillow from SQL
+
+# def new_zillow_data():
+#     '''
+#     This function reads the Bed/Bath count, Finished Sq Ft, Taxable Value, Year Built, and Amount Taxed
+#     from the 2017 Properties Zillow data from the Codeup SQL server into a df.
+#     '''
+#     sql_query = '''SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+# FROM properties_2017
+# LEFT JOIN propertylandusetype USING (propertylandusetypeid)
+# WHERE propertylandusedesc IN ("Single Family Residential", "Inferred Single Family Residential");'''
+    
+#     # Read in DataFrame from Codeup db.
+#     df = pd.read_sql(sql_query, get_connection('zillow'))
+    
+#     return df
+
+# #----------------------------------------------------------#
+# #def function + csv
+
+# def get_zillow_data():
+#     '''
+#     This function reads in zillow data from the Codeup database, writes data to
+#     a csv file if a local file does not exist, and returns a df.
+#     '''
+#     if os.path.isfile('zillow.csv'):
+        
+#         # If csv file exists read in data from csv file.
+#         df = pd.read_csv('zillow.csv', index_col=0)
+        
+#     else:
+        
+#         # Read fresh data from db into a DataFrame
+#         df = new_zillow_data()
+        
+#         # Cache data
+#         df.to_csv('zillow.csv')
+        
+#     return df
