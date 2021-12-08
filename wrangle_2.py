7fca3e5f0e053b455987a7476915e889b02b04ad
Daniel Northcutt <daniel.h.northcutt@gmail.com>
2021-12-08 16:15:26 -0600

    OG wrangle

---
+++ wrangle_2.py
@@ -0,0 +1,336 @@
+#### This was my original wrangle file I accidently saved over using the same .py name##
+import pandas as pd
+import numpy as np
+import env
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+'''Wrangles data from Zillow Database'''
+
+##################################################Wrangle.py###################################################
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+from env import user, password, host
+
+#**************************************************Acquire*******************************************************
+
+def acquire_zillow():
+    ''' Acquire data from Zillow using env imports and rename columns'''
+    
+    url = f"mysql+pymysql://{user}:{password}@{host}/zillow"
+    
+    query = """
+            
+    SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    FROM properties_2017
+    LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+    WHERE propertylandusedesc IN ("Single Family Residential",                       
+                                  "Inferred Single Family Residential")"""
+
+    # get dataframe of data
+    df = pd.read_sql(query, url)
+    
+    
+    # renaming column names to one's I like better
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'area',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'year_built',})
+    return df
+
+#**************************************************Remove Outliers*******************************************************
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+#**************************************************Distributions*******************************************************
+
+def get_hist(df):
+    ''' Gets histographs of acquired continuous variables'''
+    
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = [col for col in df.columns if col not in ['fips', 'year_built']]
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot nos should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # turn off scientific notation
+        plt.ticklabel_format(useOffset=False)
+
+        plt.tight_layout()
+
+    plt.show()
+        
+        
+def get_box(df):
+    ''' Gets boxplots of acquired continuous variables'''
+    
+    # List of columns
+    cols = ['bedrooms', 'bathrooms', 'area', 'tax_value', 'taxamount']
+
+    plt.figure(figsize=(16, 3))
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display boxplot for column.
+        sns.boxplot(data=df[[col]])
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # sets proper spacing between plots
+        plt.tight_layout()
+
+    plt.show()
+        
+#**************************************************Prepare*******************************************************
+
+def prepare_zillow(df):
+    ''' Prepare zillow data for exploration'''
+
+    # removing outliers
+    df = remove_outliers(df, 1.5, ['bedrooms', 'bathrooms', 'area', 'tax_value', 'taxamount'])
+    
+    # get distributions of numeric data
+    get_hist(df)
+    get_box(df)
+    
+    # converting column datatypes
+    df.fips = df.fips.astype(object)
+    df.year_built = df.year_built.astype(object)
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    # impute year built using mode
+    imputer = SimpleImputer(strategy='median')
+
+    imputer.fit(train[['year_built']])
+
+    train[['year_built']] = imputer.transform(train[['year_built']])
+    validate[['year_built']] = imputer.transform(validate[['year_built']])
+    test[['year_built']] = imputer.transform(test[['year_built']])       
+    
+    return train, validate, test    
+
+#**************************************************Wrangle*******************************************************
+
+def wrangle_zillow():
+    '''Acquire and prepare data from Zillow database for explore'''
+    train, validate, test = prepare_zillow(acquire_zillow())
+    
+    return train, validate, test
+
+
+#********************************************************************************************************
+#Scaling Code:
+
+def add_scaled_columns(train, validate, test, scaler, columns_to_scale):
+    
+    # new column names
+    new_column_names = [c + '_scaled' for c in columns_to_scale]
+    
+    # Fit the scaler on the train
+    scaler.fit(train[columns_to_scale])
+    
+    # transform train validate and test
+    train = pd.concat([
+        train,
+        pd.DataFrame(scaler.transform(train[columns_to_scale]), columns=new_column_names, index=train.index),
+    ], axis=1)
+    
+    validate = pd.concat([
+        validate,
+        pd.DataFrame(scaler.transform(validate[columns_to_scale]), columns=new_column_names, index=validate.index),
+    ], axis=1)
+    
+    
+    test = pd.concat([
+        test,
+        pd.DataFrame(scaler.transform(test[columns_to_scale]), columns=new_column_names, index=test.index),
+    ], axis=1)
+    
+    return train, validate, test
+
+    #************************ Standard Scaler*******************
+
+# Exercise Review Code:
+
+def Standard_Scaler(X_train, X_validate, X_test):
+    """
+    Takes in X_train, X_validate and X_test dfs with numeric values only
+    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs
+    """
+
+    scaler = sklearn.preprocessing.StandardScaler().fit(X_train)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
+    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
+    
+    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
+
+
+    #************************ Min/Max Scaler*******************
+def Min_Max_Scaler(X_train, X_validate, X_test):
+    """
+    Takes in X_train, X_validate and X_test dfs with numeric values only
+    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs 
+    """
+    scaler = sklearn.preprocessing.MinMaxScaler().fit(X_train)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
+    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
+    
+    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
+
+#*************************** TELCO WRANGLE ************************
+def new_telco_charge_data():
+    '''
+    Returns telco_charge into a dataframe
+    '''
+    sql_query = '''select customer_id, monthly_charges, tenure, total_charges from customers
+    join internet_service_types using(internet_service_type_id)
+    join contract_types using(contract_type_id)
+    join payment_types using(payment_type_id)
+    where contract_type_id = 3'''
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    return df 
+
+def get_telco_charge_data():
+    '''get connection, returns telco_charge into a dataframe and creates a csv for us'''
+    if os.path.isfile('telco_charge.csv'):
+        df = pd.read_csv('telco_charge.csv', index_col=0)
+    else:
+        df = new_telco_charge_data()
+        df.to_csv('telco_charge.csv')
+    return df
+
+def clean_telco(df):
+    '''cleans our data'''
+    df = df.replace(r'^\s*$', np.NaN, regex=True)
+    df.total_charges = pd.to_numeric(df.total_charges, errors='coerce').astype('float64')
+    df.total_charges = df.total_charges.fillna(value=df.total_charges.mean()).astype('float64')
+    df = df.set_index("customer_id")
+    return df 
+
+
+def split_telco(df):
+    '''
+    Takes in a cleaned df of telco data and splits the data appropriatly into train, validate, and test.
+    '''
+    
+    train_val, test = train_test_split(df, train_size =  0.8, random_state = 123)
+    train, validate = train_test_split(train_val, train_size =  0.7, random_state = 123)
+    return train, validate, test
+
+def wrangle_telco():
+    '''acquire and our dataframe, returns a df'''
+    df = clean_telco(get_telco_charge_data())
+    train, validate, test = split_telco(df)
+    return train, validate, test
+
+
+
+
+
+
+# #----------------------------------------------------------#
+# #get connection
+
+# def get_connection(db, user=env.user, host=env.host, password=env.password):
+#     connection_info = f'mysql+pymysql://{user}:{password}@{host}/{db}'
+#     return connection_info
+
+
+# #----------------------------------------------------------#
+# #get zillow from SQL
+
+# def new_zillow_data():
+#     '''
+#     This function reads the Bed/Bath count, Finished Sq Ft, Taxable Value, Year Built, and Amount Taxed
+#     from the 2017 Properties Zillow data from the Codeup SQL server into a df.
+#     '''
+#     sql_query = '''SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+# FROM properties_2017
+# LEFT JOIN propertylandusetype USING (propertylandusetypeid)
+# WHERE propertylandusedesc IN ("Single Family Residential", "Inferred Single Family Residential");'''
+    
+#     # Read in DataFrame from Codeup db.
+#     df = pd.read_sql(sql_query, get_connection('zillow'))
+    
+#     return df
+
+# #----------------------------------------------------------#
+# #def function + csv
+
+# def get_zillow_data():
+#     '''
+#     This function reads in zillow data from the Codeup database, writes data to
+#     a csv file if a local file does not exist, and returns a df.
+#     '''
+#     if os.path.isfile('zillow.csv'):
+        
+#         # If csv file exists read in data from csv file.
+#         df = pd.read_csv('zillow.csv', index_col=0)
+        
+#     else:
+        
+#         # Read fresh data from db into a DataFrame
+#         df = new_zillow_data()
+        
+#         # Cache data
+#         df.to_csv('zillow.csv')
+        
+#     return df
